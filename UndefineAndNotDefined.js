e7cd5e6fe1cd31f2c3132636eec0de7436042805
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-09-07 03:20:48 +0530

    sortest program and undfined and not define

---
+++ UndefineAndNotDefined.js
@@ -0,0 +1,16 @@
+// var a  ; 
+// console.log(a) ; 
+// a  = 20 ;
+
+// if(a === undefined) {
+//     console.log("a is undefined") ;
+// }else {
+//     console.log(" a is not undefined") ;
+// }
+ 
+var a ; 
+console.log(a) ; 
+a = 10 ; 
+console.log(a) ; 
+a = "hellow world" ; 
+console.log(a)  ;
