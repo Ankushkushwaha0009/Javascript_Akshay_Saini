127bb7248a760f8c86c59253861131221f7af25e
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-09-07 00:23:46 +0530

    added Hoisting.js

---
+++ Hoisting.js
@@ -0,0 +1,34 @@
+// var x = 7 ; 
+
+// function getName() {
+//     console.log("namste Javacsript")  ; 
+// }
+
+// getName() ; 
+// console.log(x) ; 
+
+
+// getName() ; 
+// console.log(x) ; 
+
+// var x = 10 ; 
+
+// function getName() {
+//     console.log("namste Javacsript")  ; 
+// }
+
+// console.log(getName) ; //it will print the whole functions  ....
+// console.log(x) ;  // x is not defined 
+// // var x = 10 ; 
+
+// function getName() {
+//     console.log("namste Javacsript")  ; 
+// }
+
+console.log(getName) ; //it will print the whole functions ....
+var x = 10 ;
+
+function getName() {
+    console.log("Namste Javacsript")  ; 
+}
+
