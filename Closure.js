419721a3e2c6cba7beb6cf3463a26c57e5f03565
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-09-10 02:07:20 +0530

    closues some more eg

--- Closure.js
+++ Closure.js
@@ -1,13 +1,40 @@
-function outer() {
-  let count = 0; // outer variable
-  function inner() {
-    count++; // inner function "closes over" count
-    console.log(count);
-  }
-  return inner;
-}
+/*
+ a function along with lexical scope bundle together forms a closure
+*/
 
-const counter = outer(); // outer() has already finished executing
-counter(); // 1
-counter(); // 2
-counter(); // 3
+// function outer() {
+//   let count = 0; // outer variable
+//   function inner() {
+//     count++; // inner function "closes over" count
+//     console.log(count);
+//   }
+//   return inner;
+// }
+
+// const counter = outer(); // outer() has already finished executing
+// counter(); // 1
+// counter(); // 2
+// counter(); // 3
+
+//---------------------------
+
+// function x() {
+//   var a = 9;
+//   function y() {
+//     console.log(a);
+//   }
+//   y();
+// }
+// x();
+
+// returning the whole fucntions  ...
+function x() {
+  var a = 9;
+  function y() {
+    console.log(a);
+  }
+  return y ; 
+}
+var z = x() ; 
+console.log(z) ; 
+z() ; // here it will print the 9 becuase it is remeber it lexical scope we will not get error
