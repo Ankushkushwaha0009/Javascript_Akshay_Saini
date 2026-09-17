e648bf7153840b70c6762359201ab9f73f0005c7
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-09-18 02:03:20 +0530

    Closure proper eg

---
+++ ClosusreRealLifeEg.js
@@ -0,0 +1,31 @@
+function createBankAccount(initialBalance) {
+  let balance = initialBalance; // Private variable
+
+  return {
+    deposit(amount) {
+      if (amount > 0) {
+        balance += amount;
+        return `Deposited $${amount}. New balance: $${balance}`;
+      }
+    },
+    withdraw(amount) {
+      if (amount <= balance) {
+        balance -= amount;
+        return `Withdrew $${amount}. Remaining balance: $${balance}`;
+      }
+      return "Insufficient funds!";
+    },
+    getBalance() {
+      return balance;
+    }
+  };
+}
+
+const myAccount = createBankAccount(100);
+
+console.log(myAccount) ; 
+
+console.log(myAccount.deposit(50));   // "Deposited $50. New balance: $150"
+console.log(myAccount.withdraw(30));  // "Withdrew $30. Remaining balance: $120"
+console.log(myAccount.balance);       // undefined (Direct access is blocked!)
+console.log(myAccount.getBalance());  // 120
