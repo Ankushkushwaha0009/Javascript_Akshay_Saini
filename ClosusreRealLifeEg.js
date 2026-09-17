768cf00558421bbaa9e0c807d399f5ed99daf303
Ankush kushwaha <ankushkushwaha2829@gmail.com>
2026-09-18 03:58:18 +0530

    some more eg

--- ClosusreRealLifeEg.js
+++ ClosusreRealLifeEg.js
@@ -1,31 +1,46 @@
-function createBankAccount(initialBalance) {
-  let balance = initialBalance; // Private variable
+// function createBankAccount(initialBalance) {
+//   let balance = initialBalance; // Private variable
 
-  return {
-    deposit(amount) {
-      if (amount > 0) {
-        balance += amount;
-        return `Deposited $${amount}. New balance: $${balance}`;
-      }
-    },
-    withdraw(amount) {
-      if (amount <= balance) {
-        balance -= amount;
-        return `Withdrew $${amount}. Remaining balance: $${balance}`;
-      }
-      return "Insufficient funds!";
-    },
-    getBalance() {
-      return balance;
-    }
-  };
+//   return {
+//     deposit(amount) {
+//       if (amount > 0) {
+//         balance += amount;
+//         return `Deposited $${amount}. New balance: $${balance}`;
+//       }
+//     },
+//     withdraw(amount) {
+//       if (amount <= balance) {
+//         balance -= amount;
+//         return `Withdrew $${amount}. Remaining balance: $${balance}`;
+//       }
+//       return "Insufficient funds!";
+//     },
+//     getBalance() {
+//       return balance;
+//     }
+//   };
+// }
+
+// const myAccount = createBankAccount(100);
+
+// console.log(myAccount) ; 
+
+// console.log(myAccount.deposit(50));   // "Deposited $50. New balance: $150"
+// console.log(myAccount.withdraw(30));  // "Withdrew $30. Remaining balance: $120"
+// console.log(myAccount.balance);       // undefined (Direct access is blocked!)
+// console.log(myAccount.getBalance());  // 120
+
+//------------------------------------------------
+
+function outer() {
+    let x = 10;
+    return function() {
+        return x++;
+    };
 }
 
-const myAccount = createBankAccount(100);
+const fn = outer(); //one function will be there in this 
 
-console.log(myAccount) ; 
-
-console.log(myAccount.deposit(50));   // "Deposited $50. New balance: $150"
-console.log(myAccount.withdraw(30));  // "Withdrew $30. Remaining balance: $120"
-console.log(myAccount.balance);       // undefined (Direct access is blocked!)
-console.log(myAccount.getBalance());  // 120
+console.log(fn()); //10
+console.log(fn()); //11
+console.log(fn());//12
